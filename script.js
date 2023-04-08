ca392de14966b60fd12bf489fcfca11e22b1b750
Shubham Upadhyay <iamshubhamupadhyaya@gmail.com>
2023-04-08 11:47:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function showData() {
     myArr[0] = "First Name: " + document.getElementById('fname').value;
 	myArr[1] = "Last Name: " +document.getElementById('lname').value;
 	myArr[2] = "Phone Number: " + document.getElementById('number').value;
-	myArr[3] = "Email Id: " + document.getElementById('email').value;
+	myArr[3] = "Email ID: " + document.getElementById('email').value;
      var text = myArr[0] + "\n" + myArr[1]
 				+ "\n" + myArr[2]
 				+ "\n" + myArr[3]
